e135ce98e629aba353585df7178a2433ab61c011
Ananda Rauf <anandarauf02@gmail.com>
2024-03-05 14:58:18 +0700

    Update CMU-Dart.py

--- cmu-dart.py
+++ cmu-dart.py
@@ -1,11 +1,12 @@
-#from figma_exporter import FigmaExporter
-from dribbble_to_flutter import DribbbleToFlutter
+import requests
+from bs4 import BeautifulSoup
+
 print("------------------------------------------------------------------\n")
 
-nameprogram= "CSM Dart"
-version= "1.0.0"
-devdate= "04 May 2024"
-devby= "Ananda Rauf Maududi"
+nameprogram = "CSM Dart"
+version = "1.0.0"
+devdate = "04 May 2024"
+devby = "Ananda Rauf Maududi"
 print(nameprogram)
 print(version)
 print(devdate)
@@ -17,54 +18,56 @@ def MenuCMU():
     print("Menu CMU Dart:\n")
     print()
     print("1. Convert Figma to Dart")
-    print("2. Convert Dribble to Dart")
-    
+    print("2. Convert Dribbble to Dart")
+
+
 def FigmaToDart():
-    # Masukkan token Figma Anda di sini
-    figma_token = "YOUR_FIGMA_TOKEN"
-    
-    # Buat objek FigmaExporter
-    exporter = FigmaExporter(figma_token)
-    
-    # ID frame atau page Figma yang ingin Anda ekspor
-    file_id = "YOUR_FIGMA_FILE_ID"
-    frame_id = "YOUR_FIGMA_FRAME_ID"
-    
-    # Ekspor desain UI dari Figma ke kode Dart
-    dart_code = exporter.export_to_flutter(file_id, frame_id)
-    
-    # Hasil convert kode dart dari Figma
-    
+    # Masukkan token dan file id Figma Anda di sini
+    file_id = 'YOUR_FIGMA_FILE_ID'
+    access_token = 'YOUR_FIGMA_ACCESS_TOKEN'
+
+    # Endpoint untuk mengambil informasi tentang desain UI dari Figma
+    url = f"https://api.figma.com/v1/files/{file_id}"
+
+    # Mengirimkan request ke API Figma
+    response = requests.get(url, headers={'X-Figma-Token': access_token})
+
+    # Mengambil data desain UI dari response
+    ui_data = response.json()
     print("Berhasil convert dari Figma ke kode Dart!")
-    print(dart_code)
+    print(ui_data)
+
 
 def DribleToDart():
-    # Buat objek DribbbleToFlutter
-    dribbble_converter = DribbbleToFlutter()
-    
-    # URL shot Dribbble yang ingin Anda konversi
-    
-    dribbble_url = "YOUR_DRIBBBLE_URL"
-    
-    # Ekspor desain UI dari Dribbble ke kode Dart
-    dart_code = dribbble_converter.convert_url_to_flutter(dribbble_url)
-    
-    # Hasil convert kode dart dari Dribbble
-    print(dart_code)
+    # URL halaman Dribbble yang berisi desain UI
+    url = 'https://dribbble.com/shots/12345678'
+
+    # Mengirimkan request ke halaman Dribbble
+    response = requests.get(url)
+
+    # Mengambil HTML halaman
+    html = response.text
+
+    # Membuat objek BeautifulSoup
+    soup = BeautifulSoup(html, 'html.parser')
+
+    # Mengambil informasi desain dari halaman menggunakan BeautifulSoup
+    # Anda perlu menyesuaikan ini sesuai dengan struktur HTML dari halaman Dribbble
+    # Misalnya, untuk mengambil judul desain:
+    title = soup.find('h1', class_='shot-title').text
+    print("Berhasil convert dari Dribble ke kode Dart!", title)
+
 
 MenuCMU()
 
-pilih= int(input("Pilih nomor menu: "))
+pilih = int(input("Pilih nomor menu: "))
 
-if pilih== 1:
+if pilih == 1:
     FigmaToDart()
 
-elif pilih==2:
+elif pilih == 2:
     DribleToDart()
 
 else:
     print("Menu tidak tersedia!")
-    exit
-    
-     
-
+    exit()
